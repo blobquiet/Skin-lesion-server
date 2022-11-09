ddfa081869f7ecaf95673c39cae10e6955920f57
blobquiet <soquiet@hotmail.com>
2022-11-09 18:24:54 +0100

    making prediction

--- server/src/app.py
+++ server/src/app.py
@@ -5,7 +5,9 @@ from firebase_admin import credentials, firestore, initialize_app
 import torch
 import numpy as np
 from joblib import load
-import pandas as pd
+
+import json
+import yaml
 
 from PIL import Image
 
@@ -22,6 +24,42 @@ todo_ref = db.collection('skin-lesions')
 def format_server_time():
     server_time = time.localtime()
     return time.strftime("%I:%M:%S %p", server_time)
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    upload_dir = "images"
+    file_names = []
+    for key in request.files:
+        app.logger.info(key)
+        file = request.files[key]
+        picture_fn = file.filename
+        file_names.append(picture_fn)
+        picture_path = os.path.join(upload_dir, picture_fn)
+        try:
+            file.save(picture_path)
+        except:
+            print("save fail: " + picture_path)
+    app.logger.info(key)
+    return json.dumps({"filename": [f for f in file_names]})
+
+
+@app.route('/saveimage', methods=['POST'])
+def upload_test():
+    upload_dir = "images"
+    file_names = []
+    for key in request.files:
+        app.logger.info(file_names)
+        file = request.files[key]
+        picture_fn = file.filename
+        file_names.append(picture_fn)
+        picture_path = os.path.join(upload_dir, picture_fn)
+        try:
+            file.save(picture_path)
+        except:
+            print("save fail: " + picture_path)
+    app.logger.info(file_names)
+    return json.dumps({"filename": [f for f in file_names]})
 
 
 @app.route('/')
@@ -45,8 +83,8 @@ def create():
         return f"An Error Occurred: {e}"
 
 
-@app.route("/predict", methods=['POST'])
-def predict():
+@app.route("/predict_test", methods=['POST'])
+def predict_test():
 
     try:
         # Load torch model
@@ -103,20 +141,20 @@ def test():
 @app.route("/testmeta", methods=['POST'])
 def testmeta():
     try:
-        #file = request.files['file'].stream
-        #file = request.json['file']
+        # file = request.files['file'].stream
+        # file = request.json['file']
         file = request.files['file'].stream
 
         age = request.form.get('age')
         sex = request.form.get('sex')
         location = request.form.get('location')
-        #age = request.json['age']
-        #sex = request.json['sex']
-        #location = request.json['location']
+        # age = request.json['age']
+        # sex = request.json['sex']
+        # location = request.json['location']
 
         # load metadata encoder
-        #hot_enconder = load('hot_enconder.joblib')
-        #meta = [age, sex, location]
+        # hot_enconder = load('hot_enconder.joblib')
+        # meta = [age, sex, location]
         # onehot = hot_enconder.transform(
         #    [meta]).toarray()
         return {
@@ -129,34 +167,55 @@ def testmeta():
         return f"An Error Occurred: {e}"
 
 
-@app.route("/predictmeta", methods=['POST'])
-def predictmeta():
+@app.route("/predict", methods=['POST'])
+def predict():
 
     try:
-        # load metadata encoder
-        hot_enconder = load('hot_enconder.joblib')
-        # Load torch model
-        model = torch.jit.load('models/model_meta.zip', map_location='cpu')
-        model.eval()
-
         # load image
-        img = Image.open(request.files['file'].stream).convert(
+        img = Image.open(request.files['files'].stream).convert(
             'RGB').resize((380, 380))
 
         img = np.array(img)
         img = torch.FloatTensor(img.transpose((2, 0, 1)) / 255)
-        # load meta
-        age = request.form.get('age')
-        sex = request.form.get('sex')
-        location = request.form.get('location')
-        meta = [age, sex, location]
-        meta = hot_enconder.transform(
-            [meta]).toarray()
-        # meta = hot_enconder.transform(
-        #    [['30', 'male', 'torso']]).toarray()
-        meta = torch.tensor(meta).float()
-        # get predictions
-        preds = model(img.unsqueeze(0), meta.float()).squeeze()
+
+        # handle if server recieves metadata
+        try:
+            # load meta
+            data = request.form.get('files')
+            data = yaml.load(data, yaml.SafeLoader)
+            app.logger.info(data)
+            app.logger.info(data['age'])
+            age = data['age']
+            sex = data['sex']
+            location = data['location']
+            meta = [age, sex, location]
+            # Load torch model
+            model = torch.jit.load('models/model_meta.zip', map_location='cpu')
+            model.eval()
+            # load metadata encoder
+            hot_enconder = load('hot_enconder.joblib')
+            meta = hot_enconder.transform(
+                [meta]).toarray()
+            # sample
+            # meta = hot_enconder.transform(
+            #    [['30', 'male', 'torso']]).toarray()
+
+            meta = torch.tensor(meta).float()
+            app.logger.info("try")
+
+            # get predictions
+            preds = model(img.unsqueeze(0), meta.float()).squeeze()
+            app.logger.info("meta")
+        except:
+            # Load torch model
+            model = torch.jit.load('models/model.zip', map_location='cpu')
+            model.eval()
+            # get predictions
+            preds = model(img.unsqueeze(0)).squeeze()
+            app.logger.info("no meta")
+            pass
+        app.logger.info("continue")
+
         probas = torch.softmax(preds, axis=0)
 
         # apply rescaling
@@ -169,6 +228,8 @@ def predictmeta():
         ix = torch.argmax(thresholding, axis=0)
 
         labels = ['AK', 'BCC', 'BKL', 'DF', 'MEL', 'NV', 'SCC', 'UNK', 'VASC']
+
+        app.logger.info(labels[ix])
         return {
             'label': labels[ix],
             'score': thresholding[ix].item(),
